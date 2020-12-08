54fa373b7a1288912ae1273bef0dc10ce428fdc9
Jamie Corrigan <38882200+jamiecorrigan@users.noreply.github.com>
2020-12-08 21:11:54 +1000

    feat: hookup config, handlers and run the server.

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/deadlift-or-die/backend/config"
+	"github.com/deadlift-or-die/backend/handler"
+)
 
 func main() {
-	fmt.Println("Hello, world")
+	c, err := config.NewConfig()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	handler.RegisterHandlers()
+
+	log.Printf("starting server on %v:%v", c.Host, c.Port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf("%v:%v", c.Host, c.Port), nil))
 }
